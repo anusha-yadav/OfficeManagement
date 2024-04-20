f5eaa192f7e092425d1699b7c483e13370650bc7
anusha-yadav <anushayadav.1904f1@gmail.com>
2024-04-20 21:48:23 +0530

    Added changes

--- OfficeManagement.Common/ViewModels/EmployeeViewModel.cs
+++ OfficeManagement.Common/ViewModels/EmployeeViewModel.cs
@@ -6,17 +6,54 @@ using System.Threading.Tasks;
 
 namespace OfficeManagement.Common.ViewModels
 {
+    /// <summary>
+    /// EmployeeViewModel
+    /// </summary>
     public class EmployeeViewModel
     {
+        /// <summary>
+        /// EmployeeId
+        /// </summary>
         public int EmployeeId { get; set; }
+
+        /// <summary>
+        /// Name
+        /// </summary>
         public string? Name { get; set; }
+
+        /// <summary>
+        /// Experience
+        /// </summary>
         public decimal Experience { get; set; }
+
+        /// <summary>
+        /// Email
+        /// </summary>
         public string Email { get; set; }
+
+        /// <summary>
+        /// Details
+        /// </summary>
         public string Details { get; set; }
+
+        /// <summary>
+        /// SkillsetIds
+        /// </summary>
         public List<int>SkillsetIds { get; set; }
+
+        /// <summary>
+        /// DepartmentId
+        /// </summary>
         public int DepartmentId { get; set; }
+
+        /// <summary>
+        /// ProjectId
+        /// </summary>
         public int ProjectId { get; set; }
-        public List<string>? Skills {  get; set; } = new List<string>();
+
+        /// <summary>
+        /// ProjectName
+        /// </summary>
         public string? ProjectName { get; set; }
     }
 }
--- OfficeManagement.Web/Controllers/OfficeController.cs
+++ OfficeManagement.Web/Controllers/OfficeController.cs
@@ -6,20 +6,37 @@ using OfficeManagement.Services.Implementation;
 
 namespace OfficeManagement.Web.Controllers
 {
+    /// <summary>
+    /// OfficeController
+    /// </summary>
     public class OfficeController : Controller
     {
         private readonly IEmployeeService EmployeeService;
 
+        /// <summary>
+        /// OfficeController
+        /// </summary>
+        /// <param name="employeeService"></param>
         public OfficeController(IEmployeeService employeeService) 
         {
             EmployeeService = employeeService;
             
         }
+
+        /// <summary>
+        /// Index
+        /// </summary>
+        /// <returns></returns>
         public IActionResult Index()
         {
             return View();
         }
 
+        /// <summary>
+        /// AddEmployeeDialog
+        /// </summary>
+        /// <param name="employeeId"></param>
+        /// <returns></returns>
         public PartialViewResult AddEmployeeDialog(int employeeId)
         {
             EmployeeViewModel employee = new EmployeeViewModel();
@@ -34,6 +51,10 @@ namespace OfficeManagement.Web.Controllers
             return PartialView("AddOrEditEmployee", employee);
         }
 
+        /// <summary>
+        /// SaveDetails
+        /// </summary>
+        /// <param name="employee"></param>
         public void SaveDetails(EmployeeViewModel employee)
         {
             EmployeeService.SaveDetails(employee);
